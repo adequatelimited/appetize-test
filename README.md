04d447fcbfbda870d70817f88ea1098095fe40a2
Adequate Limited <matt@adequatelimited.com>
2026-01-18 14:36:08 -0500

    Initial commit: minimal Hello World iOS app

---
+++ README.md
@@ -0,0 +1,13 @@
+# Appetize.io Test App
+
+A minimal "Hello World" iOS app to test Appetize.io integration.
+
+## Structure
+
+- Simple single-view app with a blue background and "Hello World" label
+- No dependencies, no resources, no frameworks
+- Targets iOS 15.0+
+
+## Purpose
+
+Testing whether Appetize.io works with our GitHub Actions build pipeline.
---
+++ ios/HelloWorld/ViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
